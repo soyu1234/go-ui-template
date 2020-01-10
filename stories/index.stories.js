690bfa64ebb92f2ab692945280516033c2c5dadb
Furkan Kurşun <furkan.kursun@triodor.eu>
2020-01-10 11:07:24 +0300

    dark and light theme has been set up both for footer and header

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,22 +1,24 @@
 // library import section
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import '@storybook/addon-viewport/register';
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { ThemeProvider } from "@material-ui/styles";
+import "@storybook/addon-viewport/register";
 // component import section
-import { Home } from '../src/Home/index';
-import { Footer } from '../src/Footer';
-import { FooterLeft } from '../src/FooterLeft';
+import { Home } from "../src/Home/index";
+import { Footer } from "../src/Footer";
+import { FooterLeft } from "../src/FooterLeft";
+import { Header } from "../src/Header";
+import theme from "./theme";
 
-import AceEditor from 'react-ace';
+import AceEditor from "react-ace";
 
-import 'ace-builds/src-noconflict/mode-javascript';
-import 'ace-builds/src-noconflict/mode-yaml';
-import 'ace-builds/src-noconflict/theme-github';
-
+import "ace-builds/src-noconflict/mode-javascript";
+import "ace-builds/src-noconflict/mode-yaml";
+import "ace-builds/src-noconflict/theme-github";
 
 // utils imports section
-import LayoutFeatures from '../src/constants/constants';
-import data from './data';
+import LayoutFeatures from "../src/constants/constants";
+import data from "./data";
 
 // storiesOf('Home', module).add('5 in 1', () => {
 //   // Create new object from original data with different reference, so actual data won't be corrupted.
@@ -33,10 +35,32 @@ import data from './data';
 //   return <Home data={moreData} layout={LayoutFeatures.MUCH_AMOUNT_OF_CARDS} />;
 // });
 
-storiesOf('Footer', module).add('footer', () => {
-  return <Footer/>
+// storiesOf("Footer", module).add("footer", () => {
+//   return (
+//     <ThemeProvider theme={theme}>
+//       <Footer />
+//     </ThemeProvider>
+//   );
+// });
+
+storiesOf("Footer", module).add("footerLeft", () => {
+  const props = {
+    url: "https://gamedistribution.com/",
+    website: "Game Distribution",
+    privacyUrl: "https://azerion.com/business/privacy.html",
+    termsAndConditionsUrl: "https://static.gamedistribution.com/terms/both.html"
+  };
+  return (
+    <ThemeProvider theme={theme}>
+      <FooterLeft {...props} themeMode={theme.palette.type} />
+    </ThemeProvider>
+  );
 });
 
-storiesOf('Footer', module).add('footerLeft', () => {
-  return <FooterLeft/>
-})
+storiesOf("Header", module).add("header1", () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Header themeMode={theme.palette.type} />
+    </ThemeProvider>
+  );
+});
