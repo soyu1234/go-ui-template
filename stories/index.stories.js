f52bc50ba298a0a82891bfdab5b779c640983267
Furkan Kurşun <furkan.kursun@triodor.eu>
2020-01-13 18:37:21 +0300

    slate markdown editor has been implemented, not finished

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -9,25 +9,29 @@ import { Footer } from "../src/Footer";
 import { FooterLeft } from "../src/FooterLeft";
 import { Header } from "../src/Header";
 import { ReviewModal } from "../src/ReviewModal";
+import { ComponentTest } from "../src/ComponentTest";
+
 import theme from "./theme";
-
-import AceEditor from "react-ace";
-
-import "ace-builds/src-noconflict/mode-javascript";
-import "ace-builds/src-noconflict/mode-yaml";
-import "ace-builds/src-noconflict/theme-github";
 
 // utils imports section
 import LayoutFeatures from "../src/constants/constants";
 import data from "./data";
 
-// storiesOf('Home', module).add('5 in 1', () => {
-//   // Create new object from original data with different reference, so actual data won't be corrupted.
-//   const lessData = { ...data };
-//   lessData.game.amount = 5;
-//   console.log(LayoutFeatures.LESS_AMOUNT_OF_CARDS);
-//   return <Home data={lessData} layout={LayoutFeatures.LESS_AMOUNT_OF_CARDS} />;
-// });
+storiesOf("Home", module).add("5 in 1", () => {
+  // Create new object from original data with different reference, so actual data won't be corrupted.
+  const lessData = { ...data };
+  lessData.game.amount = 5;
+  console.log(LayoutFeatures.LESS_AMOUNT_OF_CARDS);
+  return <Home data={lessData} layout={LayoutFeatures.LESS_AMOUNT_OF_CARDS} />;
+});
+
+storiesOf("editor", module).add("Slate", () => {
+  const handleChange = data => {
+    glob = data;
+    console.log(glob);
+  };
+  return <ComponentTest change={data => handleChange(data)} data={data} />;
+});
 
 // storiesOf('Home', module).add('100 in 1', () => {
 //   // Create new object from original data with different reference, so actual data won't be corrupted.
@@ -73,3 +77,11 @@ storiesOf("Header", module).add("header1", () => {
 //     </ThemeProvider>
 //   );
 // });
+
+storiesOf("test component", module).add("test component", () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <ComponentTest />
+    </ThemeProvider>
+  );
+});
